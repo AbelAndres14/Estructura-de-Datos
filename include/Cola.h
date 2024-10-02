7fc68150377f4960dd4b5b212e2c429f1c1ae897
AbelAndres14 <125427229+AbelAndres14@users.noreply.github.com>
2024-10-01 19:53:03 -0600

    Add files via upload

---
+++ include/Cola.h
@@ -0,0 +1,50 @@
+#ifndef COLA_H
+#define COLA_H
+#include <iostream>
+const int TAM=100;
+
+//***************************Definicion de la clase persona*************************
+//**********************************************************************************
+class persona
+{
+private:
+    std::string nombre;
+    int edad;
+public:
+    persona();
+    void operator=(persona& x);
+    persona(std::string n, int e);
+    friend std::ostream& operator<<( std::ostream&, persona&);
+    friend std::istream& operator>>( std::istream&, persona&);
+    bool operator==(persona&p){
+        return p.nombre==nombre&&p.edad==edad;
+    }
+    std::string getNombre() const { return nombre; }
+};
+//***************************************************************************************
+
+
+
+//****************************Definicion de la clase Pila****************************
+//***********************************************************************************
+class Cola
+{
+private:
+    persona datos[TAM];
+    int ult;
+    bool elimina(int pos);
+    int inserta(persona& elem, int pos);
+public:
+    Cola():ult(-1) {}
+    bool vacia()const;
+    bool llena()const;
+    int ultimo()const;
+    friend std::ostream& operator<<(std::ostream & o, Cola& L);
+    void enqueue(persona& elem);
+    persona& dequeue();
+    int busquedaLineal(persona&);
+    void buscarSolicitud(persona& alumno);
+};
+//*************************************************************************************
+
+#endif // COLA_H
